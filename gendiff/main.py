f6cd812306155f886fb91dc50cf5ce6732d64a3e
d.proskuryakov <d.proskuryakov@medpoint24.ru>
2026-06-26 19:16:51 +0300

    add format option to help message

--- gendiff/main.py
+++ gendiff/main.py
@@ -7,4 +7,5 @@ def start():
                      "and shows a difference."))
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
     parser.parse_args()
