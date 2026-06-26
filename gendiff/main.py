940b9ff7ea88926c26573e1238301babd5e1748b
d.proskuryakov <d.proskuryakov@medpoint24.ru>
2026-06-26 19:32:38 +0300

    read json

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,11 +1,16 @@
 import argparse
+import json
 
 
 def start():
     parser = argparse.ArgumentParser(
         description=("Compares two configuration files "
                      "and shows a difference."))
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
     parser.add_argument("-f", "--format", help="set format of output")
-    parser.parse_args()
+    args = parser.parse_args()
+    f1 = json.load(open(args.first_file))
+    f2 = json.load(open(args.second_file))
+    print(f1)
+    print(f2)
